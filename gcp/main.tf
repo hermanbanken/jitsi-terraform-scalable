79f477de05d49bbbc72567e48e0121b66edc5e5b
Herman Banken <hermanbanken@gmail.com>
2020-06-10 17:48:02 +0200

    Skip HAProxy for now

--- gcp/main.tf
+++ gcp/main.tf
@@ -3,14 +3,33 @@ provider "google" {
   version = "~> 3.0"
 }
 
+resource "random_id" "rnd" {
+  byte_length = 4
+}
+
+locals {
+  shard_id = random_id.rnd
+}
+
+resource "google_dns_managed_zone" "default" {
+  # Import this resource!
+  lifecycle {
+    prevent_destroy = true # imported, do not delete
+  }
+}
+
+resource "google_dns_record_set" "meet" {
+  name = "meet-${locals.shard_id}"
+  type = "A"
+  ttl  = 300
+  managed_zone = google_dns_managed_zone.default.name
+  rrdatas = [google_compute_instance_from_template.meet.network_interface[0].access_config[0].nat_ip]
+}
+
 locals {
   shared_script = templatefile("${path.module}/scripts/jitsi-shared.sh.tpl", {
     jitsi_hostname = var.jitsi_hostname
     jitsi_jvbsecret = "random" // todo
-  })
-  haproxy_script = templatefile("${path.module}/scripts/jitsi-haproxy.sh.tpl", {
-    jitsi_hostname = var.jitsi_hostname
-    jitsi_bucket_certificates = var.jitsi_bucket_certificates
   })
   meet_script = templatefile("${path.module}/scripts/jitsi-meet.sh.tpl", {
     jitsi_hostname = var.jitsi_hostname
@@ -23,4 +42,3 @@ locals {
     jitsi_xmpp_auth_password = "todo"
   })
 }
-
--- instance_groups.tf
+++ instance_groups.tf
@@ -1,28 +1,15 @@
-module "mig-haproxy" {
-  # Docs: https://registry.terraform.io/modules/GoogleCloudPlatform/managed-instance-group/google/1.0.0
-  source            = "github.com/terraform-google-modules/terraform-google-vm/modules/mig"
-  region            = var.jitsi_shard.region
-  name              = "jitsi-shard-${var.jitsi_shard.id}-haproxy"
-	instance_template = google_compute_instance_template.haproxy.output.selfLink
-  minReplicas       = 1
-  maxReplicas       = 2
-}
-
-module "mig-meet" {
-  # Docs: https://registry.terraform.io/modules/GoogleCloudPlatform/managed-instance-group/google/1.0.0
-  source            = "github.com/terraform-google-modules/terraform-google-vm/modules/mig"
-  region            = var.jitsi_shard.region
-  name              = "jitsi-shard-${var.jitsi_shard.id}-meet"
-	instance_template = google_compute_instance_template.meet.output.selfLink
-  minReplicas       = 1
-  maxReplicas       = 2
+resource "google_compute_instance_from_template" "meet" {
+  name         = "jitsi-${locals.shard_id}-meet"
+  source_instance_template = google_compute_instance_template.meet.id
+  machine_type = var.jitsi_shard.machineType
+  zone         = var.jitsi_shard.zone
 }
 
 module "mig-jvb" {
   # Docs: https://registry.terraform.io/modules/GoogleCloudPlatform/managed-instance-group/google/1.0.0
   source            = "github.com/terraform-google-modules/terraform-google-vm/modules/mig"
   region            = var.jitsi_shard.region
-  name              = "jitsi-shard-${var.jitsi_shard.id}-jvb"
+  name              = "jitsi-${locals.shard_id}-jvb"
 	instance_template = google_compute_instance_template.jvb.output.selfLink
   minReplicas       = 2
   maxReplicas       = 20
